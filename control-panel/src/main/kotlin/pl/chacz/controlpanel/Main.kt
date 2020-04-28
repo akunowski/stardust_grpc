21ca33e36fe31d4e8d15f88861bbede87ecb8063
Adrian Kunowski <adrian.kunowski@allegro.pl>
2020-04-28 19:26:37 +0200

    Create control panel

---
+++ control-panel/src/main/kotlin/pl/chacz/controlpanel/Main.kt
@@ -0,0 +1,39 @@
+package pl.chacz.controlpanel
+
+import com.google.protobuf.Empty
+import io.grpc.ManagedChannelBuilder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.asExecutor
+import kotlinx.coroutines.async
+import pl.chacz.deathstar.DeathStarGrpcKt
+import pl.chacz.deathstar.DestroyReply
+import pl.chacz.deathstar.DestroyRequest
+import pl.chacz.galaxy.GalaxyGrpcKt
+
+
+suspend fun main()  {
+    val galaxyClient = GalaxyGrpcKt.GalaxyCoroutineStub(
+        ManagedChannelBuilder
+            .forAddress("localhost", 8802)
+            .usePlaintext()
+            .executor(Dispatchers.Default.asExecutor())
+            .build())
+
+    val deathStarClient = DeathStarGrpcKt.DeathStarCoroutineStub(
+        ManagedChannelBuilder
+            .forAddress("localhost", 8801)
+            .usePlaintext()
+            .executor(Dispatchers.Default.asExecutor())
+            .build())
+
+
+    val planet = GlobalScope.async { galaxyClient.findPlanet(Empty.getDefaultInstance()) }
+    val destroyRequest = DestroyRequest.newBuilder().setName(planet.await().id).build()
+
+    val removedStatus = GlobalScope.async {
+        deathStarClient.destroy(destroyRequest)
+    }
+
+    println(removedStatus.await().status)
+}
